b6bbe308f2dc9b32238344df9823abc8ed4788bf
aminul <rahathssn@gmail.com>
2025-01-24 16:25:38 +0600

    added Executing Relation Queries

--- src/create.ts
+++ src/create.ts
@@ -1,102 +1,43 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const main = async () => {
-  //   const result = await prisma.post.create({
-  //     data: {
-  //       title: "This is title",
-  //       content: "This is content...",
-  //       authorName: "Rahat Khan",
-  //     },
-  //   });
+  // const createUser = await prisma.user.create({
+  //   data: {
+  //     username: "user 2",
+  //     email: "user2@mail.com",
+  //     role: UserRole.user,
+  //   },
+  // });
 
-  const createMany = await prisma.post.createMany({
-    data: [
-      {
-        title: "Title 1",
-        content: "Content 1",
-        authorName: "Author 1",
+  // const createProfile = await prisma.profile.create({
+  //   data: {
+  //     bio: "This is bio",
+  //     userId: 1,
+  //   },
+  // });
+
+  // const createCategory = await prisma.category.create({
+  //   data: {
+  //     name: "Software Enginear",
+  //   },
+  // });
+
+  const createPost = await prisma.post.create({
+    data: {
+      title: "This is title 6",
+      content: "This is a content of the post 6",
+      authorId: 2,
+      postCategory: {
+        create: [{ categoryId: 2 }, { categoryId: 3 }, { categoryId: 4 }],
       },
-      {
-        title: "Title 2",
-        content: "Content 2",
-        authorName: "Author 2",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-      {
-        title: "Title 3",
-        content: "Content 3",
-        authorName: "Author 3",
-      },
-    ],
+    },
+    include: {
+      postCategory: true,
+    },
   });
-
-  console.log({ createMany });
+  console.log(JSON.stringify(createPost, null, 2));
 };
 
 main();
